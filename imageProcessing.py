a862d08a48bfcbea00a77622700f80ebd8e3c76d
mikillin <sergey.rogachevsky@gmail.com>
2024-02-08 05:45:15 +0100

    added human detection with HOG

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,5 +1,4 @@
 import cv2
-import requests
 import numpy as np
 import urllib.request
 
@@ -17,6 +16,21 @@ def processImage():
         # display image
         cv2.imwrite("result.jpg", image)
 
+
+        HOGCV = cv2.HOGDescriptor()
+        HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        bounding_box_cordinates, weights = HOGCV.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.03)
+
+        person = 1
+        for x, y, w, h in bounding_box_cordinates:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(image, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            person += 1
+
+        cv2.putText(image, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(image, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+        cv2.imshow('output', image)
+        cv2.imwrite("result_detected.jpg", image)
 
 if __name__ == "__main__":
     processImage()
